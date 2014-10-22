ba5e25805c66a5e9f0c781e2ab04faeb1bf48f34
Ismael Ambrosi <ismaambrosi@gmail.com>
2014-10-22 01:17:47 -0200

    Cambios en el form de workshops
    3 dropdowns en lugar de checkboxes para seleccionar los workshops

--- src/frontend/workshops-form.php
+++ src/frontend/workshops-form.php
@@ -5,12 +5,12 @@
         <div class="sixteen columns">
             <div class="five columns">
                 <label style="text-align: left;" for="">Nombre</label>
-                <input style="text-align: left;" type="text" name="nombre"/>
+                <input style="text-align: left;" type="text" name="nombre" required/>
             </div>
 
             <div class="five columns">
                 <label style="text-align: left;" for="">e-mail</label>
-                <input style="text-align: left;" type="email" name="email"/>
+                <input style="text-align: left;" type="email" name="email" placeholder="Dirección del registro." required/>
             </div>
         </div>
     </fieldset>
@@ -18,21 +18,22 @@
 
     <fieldset>
         <div class="sixteen columns">
-            <?php
-            $workshops = include(__DIR__.'/workshops.php');
-            foreach ($workshops as $pos => $workshop): ?>
-            <label class="five columns" style="text-align: left;">
-                <input type="checkbox" name="workshops[]" value="<?= $workshop['key'] ?>"/> <span><?= $workshop['titulo'] ?></span>
-            </label>
-            <?php if (0 === (($pos + 1) % 3)) : ?>
-        </div>
-        <div class="sixteen columns">
-            <?php endif; ?>
-            <?php endforeach; ?>
+            <?php $options = implode(' ', array_map(function ($workshop) {
+                return sprintf('<option value="%s">%s</option>', htmlspecialchars($workshop['key']), htmlspecialchars($workshop['titulo']));
+            }, include(__DIR__.'/workshops.php'))); ?>
+
+            <?php for ($i = 1; $i <= 3; $i++): ?>
+                <div class="five columns" style="text-align: left;">
+                    <select name="workshops[]" <?= 1 === $i ? 'required="required"' : '' ?> id="workshop<?= $i ?>">
+                        <option value="">---</option>
+                        <?= $options; ?>
+                    </select>
+                </div>
+            <?php endfor; ?>
         </div>
     </fieldset>
 
     <p class="messages"></p>
 
-    <button type="submit">Registrarme</button>
+    <button type="submit" disabled="disabled">Registrarme</button>
 </form>
