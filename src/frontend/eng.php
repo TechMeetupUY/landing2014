dad55680154970399e102977aa9680fab20a1f2c
Santiago Camelo <trikanna@gmail.com>
2014-10-28 02:24:11 -0200

    cleanup

--- src/frontend/eng.php
+++ src/frontend/eng.php
@@ -155,35 +155,6 @@
 	<div class="yellow-wrapper">
 		<div class="container container-with-margin" id="compra">
 		    <section class="sixteen columns compra clearfix">
-				<!--
-		        <h1></h1>
-		        <p></p>
-		        
-				<br>
-
-				<div style="width:100%; text-align:left;height:300px" >
-					
-					<iframe style="position: relative!important;" src="//eventbrite.com/tickets-external?eid=12222121695&ref=etckt" 
-						frameborder="0" 
-						height="247" 
-						width="100%" 
-						vspace="0" 
-						hspace="0" 
-						marginheight="5" 
-						marginwidth="5" 
-						scrolling="auto" 
-						allowtransparency="true" >
-					</iframe>
-
-				</div>
-
-				<div style="font-size:15px; padding:5px 0 5px; margin:2px; width:100%; text-align:center;" >
-					<a style="color:black; text-decoration:none;" target="_blank" href="http://www.eventbrite.com/r/etckt">Online ticket sale</a> <span style="color:black;">via</span> <a style="color:black; text-decoration:none;font-weight:bold;" target="_blank" href="http://www.eventbrite.com?ref=etckt">Eventbrite</a>
-				</div>
-
-				<br>
-
-				-->
 				
 		        <h1 style="color: black;">Buy Your ticket</h1>
 		        <h3 style="color: black;">Book your place at the 2014 TechMeetup now!</h3>
