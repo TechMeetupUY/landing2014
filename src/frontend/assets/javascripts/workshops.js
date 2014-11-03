9eaabc213b2a0e76f976d6b7ff32aa60d8a7b3b4
Ismael Ambrosi <ismaambrosi@gmail.com>
2014-11-03 20:44:56 -0200

    Cambio de texto en botón después del submit

--- src/frontend/assets/javascripts/workshops.js
+++ src/frontend/assets/javascripts/workshops.js
@@ -6,6 +6,7 @@
     app.controller('FormCtrl', ['$scope', '$http', function ($scope, $http) {
         $scope.model = {};
         $scope.messages = [];
+        $scope.registrando = false;
 
         $scope.submit = function () {
             $scope.messages.length = 0;
@@ -15,8 +16,10 @@
                 url: 'registro.php',
                 data: $scope.model
             });
+            $scope.registrando = true;
 
             promise.success(function () {
+                $scope.registrando = false;
                 $scope.messages.push({
                     error: false,
                     text: '¡Registro completo! Muchas gracias.'
@@ -24,6 +27,7 @@
             });
 
             promise.error(function (result) {
+                $scope.registrando = false;
                 result.errors.forEach(function (error) {
                     $scope.messages.push({
                         error: true,
