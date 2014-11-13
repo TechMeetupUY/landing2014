8efa784ac57d9b2840e671a0e352484f99fb7135
Martin Loy <martinloy.uy@gmail.com>
2014-11-12 22:39:08 -0200

    twitter de luisvinay

--- src/frontend/workshops.php
+++ src/frontend/workshops.php
@@ -120,7 +120,7 @@ Se asumen conocimientos generales de programación en un lenguaje como Java o Ru
 		'titulo' => 'OpenStack',
 		'descripcion' => 'OpenStack es el proyecto más activo de los últimos años en el mundo del Open Source. Las empresas más grandes del planeta han puesto sus ojos en el. Todos los días aparecen nuevas funcionalidades y líneas de código. 
 		<br>Ésta charla es una presentación de OpenStack, qué es, para que se puede usar y qué camino tomar para entrar al mundo del cloud computing basado en Open Source.',
-		'trainers' => '<a href="https://twitter.com/DiegoWoitasen">Diego Woitasen</a> | <a href="https://www.linkedin.com/in/luisvinay">Luis Vinay</a> by <a href="http://www.meetup.com/DevOps-MVD/">DevOps Meetup</a>',
+		'trainers' => '<a href="https://twitter.com/DiegoWoitasen">Diego Woitasen</a> | <a href="https://twitter.com/luisvinay">Luis Vinay</a> by <a href="http://www.meetup.com/DevOps-MVD/">DevOps Meetup</a>',
     'reqs' => '<ul><li>Laptop con WiFi</li>
                    <li>Cliente ssh instaldo</li>
                </ul>',
